3ff015ad096d2f2d3fda90c1061a9eb074edc86a
Timon Schmid <timon.schmid@bluewin.ch>
2023-02-13 13:05:49 +0100

    tab bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,61 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            Colors.deepPurple.shade800.withOpacity(0.8),
+            Colors.deepPurple.shade200.withOpacity(0.8),
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: const _CustomAppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.deepPurple.shade800,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.white,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite_outline), label: 'Favorites'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.play_circle_outline), label: 'Play'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people_outline), label: 'Profile'),
+            ]),
+        body: Container(),
+      ),
+    );
   }
+}
+
+class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const _CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: const Icon(Icons.grid_view_rounded),
+        actions: [
+          Container(
+              margin: const EdgeInsets.only(right: 20),
+              child: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'),
+              ))
+        ]);
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
 }
