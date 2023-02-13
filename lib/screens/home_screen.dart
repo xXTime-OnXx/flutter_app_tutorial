be3657fdd947608123cc123df166400052348b1c
Timon Schmid <timon.schmid@bluewin.ch>
2023-02-13 14:36:47 +0100

    titles and search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,27 +15,84 @@ class HomeScreen extends StatelessWidget {
             Colors.deepPurple.shade200.withOpacity(0.8),
           ])),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: const _CustomAppBar(),
-        bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.deepPurple.shade800,
-            unselectedItemColor: Colors.white,
-            selectedItemColor: Colors.white,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.favorite_outline), label: 'Favorites'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.play_circle_outline), label: 'Play'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.people_outline), label: 'Profile'),
-            ]),
-        body: Container(),
+          backgroundColor: Colors.transparent,
+          appBar: const _CustomAppBar(),
+          bottomNavigationBar: const _CustomNavBar(),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [const _DiscoverMusic()],
+            ),
+          )),
+    );
+  }
+}
+
+class _DiscoverMusic extends StatelessWidget {
+  const _DiscoverMusic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Welcome', style: Theme.of(context).textTheme.bodyLarge),
+          const SizedBox(height: 5),
+          Text(
+            'Enjoy your favorite music',
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+                isDense: true,
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Search',
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.grey.shade400),
+                prefixIcon: Icon(Icons.search, color: Colors.grey.shade400),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: BorderSide.none)),
+          )
+        ],
       ),
     );
+  }
+}
+
+class _CustomNavBar extends StatelessWidget {
+  const _CustomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple.shade800,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.white,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline), label: 'Favorites'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline), label: 'Play'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline), label: 'Profile'),
+        ]);
   }
 }
 
